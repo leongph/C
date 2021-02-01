9c1745e56e0b09b6c2e2d35db8c5ab5b15a4a4fb
Leong PH <leong@mail.com>
2021-02-01 12:13:25 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello!\n3");
+    printf("Enter age:");
+    int age = 0;
+    scanf("%d",&age);
+    printf("You are %d years old \n", age);
+    return 1;
+}
